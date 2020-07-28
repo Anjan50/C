55dd58a3b4bbcefdf8dd5703897ecac326f9743d
Anjan Diyora <49095535+Anjan50@users.noreply.github.com>
2020-07-28 16:36:05 +0530

    Add files via upload

---
+++ Binary search.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+int main()
+{
+    int i, low, high, mid, x, flag, a[10];
+    x=10;
+    printf("Enter 10 integers\n");
+
+    for (i = 0; i < x; i++)
+    {
+        scanf("%d", & a[i]);
+    }
+
+    printf("Enter value to find\n");
+    scanf("%d", & flag);
+
+    low = 0;
+    high = x - 1;
+    mid = (low + high) / 2;
+
+    while (low <= high)
+    {
+        if (a[mid] < flag)
+            {
+                low = mid + 1;
+            }
+        else if (a[mid] == flag)
+            {
+            printf("%d found at location %d.\n", flag, mid + 1);
+            break;
+            }
+
+        else high = mid - 1;
+        mid = (low + high) / 2;
+    }
+
+    if (low > high)
+        {
+            printf("Not found! %d is not present in the list.\n", flag);
+        }
+    return 0;
+}
